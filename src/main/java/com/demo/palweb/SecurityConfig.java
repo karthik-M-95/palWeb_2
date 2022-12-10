913b98cc619dd8a6ce0859664875643587f15642
karthik-M-95 <karthikappu1995@gmail.com>
2022-12-10 08:26:54 +0530

    commit 2 test

--- src/main/java/com/demo/palweb/SecurityConfig.java
+++ src/main/java/com/demo/palweb/SecurityConfig.java
@@ -54,6 +54,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+		
 //	 auth.jdbcAuthentication()
 //		.dataSource(dataSource)
 //		.passwordEncoder(NoOpPasswordEncoder.getInstance())
